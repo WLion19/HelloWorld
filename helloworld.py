8eee0debde9b262b802e0ac477f1ea0b14bd83a4
WLion19 <maria.lopez.moriana98@gmail.com>
2021-12-28 20:55:25 +0100

    Hello word!

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print("hello world")
+print(25)
+greeting = input("Bienvenido al playground, quieres jugar? ")
+if greeting == "no":
+    print ("Perfecto, no me apetecia para nada.")
+elif greeting == "si":
+    print ("Pues se me pasaron las ganas.") #todo: Texto para pulir 
+name = input("Para empezar, como te llamas? ")
+print ("Bienvenid@ " + name)
