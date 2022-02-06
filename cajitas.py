43c25ade90f503e9570e0fc57953c459edd9ef9e
WLion19 <maria.lopez.moriana98@gmail.com>
2022-02-06 19:10:53 +0100

    Working with variables

---
+++ cajitas.py
@@ -0,0 +1,62 @@
+entero = 5
+decimal = 2.5
+boleano = True
+texto1 = ""
+texto2 = "Hola"
+texto3 = "Adios"
+nulo = None
+
+print(entero)
+print(type(entero))
+print(str(entero))
+print(type(str(entero)))
+print(decimal)
+print(type(decimal))
+print(boleano)
+print(type(boleano))
+print(texto1)
+print(type(texto1))
+print(nulo)
+print(type(nulo))
+
+print(entero + entero)
+print("5+5=10")
+texto4 = texto2 + texto3
+print(texto2 + texto3)
+print(texto4)
+
+
+print(texto2 + str(entero))
+print([texto2 , entero])
+
+entero = entero + 3
+print(str(entero) + "+" + str(entero) + "=" + str(entero + entero))
+
+
+print(int("2"))
+print(float("5.67"))
+print(int(5.67))
+print(float(2))
+print(entero + decimal)
+
+print(bool(entero))
+print(bool(decimal))
+print(bool(texto1))
+print(bool(texto2))
+print(bool(nulo))
+
+print()
+print(bool(-1))
+print(bool(0))
+print(bool(5))
+condicion = 0
+if condicion:
+    print("verdad")
+else:
+    print("mentira") 
+
+print(texto2.startswith("Ho"))
+print(texto2.startswith("ho"))
+print(texto2.replace("la", "lo"))
+print(texto2)
+
--- helloworld.py
+++ helloworld.py
@@ -2,8 +2,10 @@ print("hello world")
 print(25)
 greeting = input("Bienvenido al playground, quieres jugar? ")
 if greeting == "no":
-    print ("Perfecto, no me apetecia para nada.")
+    print("Perfecto, no me apetecia para nada.")
 elif greeting == "si":
-    print ("Pues se me pasaron las ganas.") #todo: Texto para pulir 
+    print("Pues se me pasaron las ganas.") #todo:Texto para pulir 
+else:
+    print("No me importa tu respuesta, siguiente...")
 name = input("Para empezar, como te llamas? ")
 print ("Bienvenid@ " + name)
