d1c391f8cdc628ddf143f151f70eb3c5c2fbfa26
WLion19 <maria.lopez.moriana98@gmail.com>
2022-02-17 19:23:02 +0100

    Limpiando codigos

--- helloworld.py
+++ helloworld.py
@@ -13,29 +13,71 @@ print("Me gustaria conocerte, ¿Que tal una ronda de preguntas?")
 #### to do
 # informacion tiene que ser correcta (estar validada) ej: age tiene que ser numero, altura float etc
 # 
-age = input("Que edad tienes? ")
-nation = input("Donde creciste? ")
-height = input("Cual es tu altura? En metros, porfavor. ")
-birthday_week = input("Cuantas semanas quedan hasta tu cumpleaños? ")  # pidieras dia y mes de cumpleañoscheck
-birthday_day = input("En que dia naciste? ")
-birthday_month = input("En que mes naciste? ")
-breath = input("Estas respirando, verdad? ") # Aceptar una respuesta valida
-
-# is_breathing = True o False
 
 print()
-print("Bien, creo haberte conocido lo sufiente. ¿No me crees? Veras...")
-print("Tu nombre es " + str(name))
+age = input("Que edad tienes? ")
+is_numeric = age.isnumeric()
+while is_numeric == False:
+    print("Este numero no es valido.")
+    age = input("Introduce otro número, por favor. ")
+    is_numeric = age.isnumeric()
 print("Tu edad es " + str(age) + " años")
-print("Conozco incluso tu altura, mides " + str(height) + " metros") # 1.55 Exactamente 2 decimales
-print("Creciste en un maravilloso lugar llamado " + str(nation))
-print("De hecho, somos grandes amigos, ya he marcado las " + str(birthday_week) + " semanas que quedan hasta tu cumpleaños en mi calendario") # los dias que quedan
-import datetime
-daysleft = datetime.datetime(int(birthday_day)) #no funciona:()
-print(daysleft)
 
-if breath == "verdad": # variable deberia ser if is_breathing
-    print("Y tenemos cosas en común, ambos respiramos ")
-if breath == "mentira":
-    print("Aunque aun no consigo entender como existes, ")
-print("Seamos amigos, disfrutemos del juego!")
+print()
+nation = input("Donde creciste? ")
+print("Interesante, asi que creciste en un maravilloso lugar llamado " + str(nation))
+
+print()
+height = input("Cual es tu altura? En metros, porfavor. ")
+try:
+    h_float = float(height) 
+    is_float = True
+   
+except ValueError:
+    print ("No es una altura valida")
+    is_float = False
+
+while is_float == False :
+    height = input("Esta altura no es valida, dame otra ")
+    try:
+        h_float = float(height)
+        is_float = True
+        print("Esta altura es valida ")
+
+    except ValueError:
+        is_float = False
+
+print(h_float)
+print("{:.2f}".format(h_float))
+
+
+print()
+birthday_day = input("En que dia naciste? ")
+birthday_month = input("En que mes naciste? ")
+birthday_year = input ("En que año naciste? Escribelo en 4 digitos ")
+
+import datetime
+x = datetime.datetime( int(birthday_year), int(birthday_month), int(birthday_day))
+print("Apuntare tu cumpleaños en mi calendario. Es el " + x + " , verdad? ")
+day_year = x.strftime("%j")
+print("Vaya! Solo quedan " + + " dias!")                                                        #!ERROR DATE
+
+#import datetime
+#daysleft = datetime.datetime(int(birthday_day)) #no funciona:()
+#print(daysleft)
+
+
+print()
+breath = input("Estas respirando? ") # Aceptar una respuesta valida
+
+while not (breath.casefold() == "si" or breath.casefold() == "no"):
+    print("Esta respuesta no es valida")
+    breath = input("Introduzca una respuesta de si o no. ")
+
+
+if breath.casefold() == "si":
+    is_breathing = True
+else:
+    is_breathing = False
+
+print(is_breathing)
